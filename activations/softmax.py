6010fb8b961eb9f4006bb15a60381f39bc05b757
Christian Singer <christian.singer@mail.schwarz>
2022-12-19 11:32:57 +0100

    - Add softmax activation function.

---
+++ activations/softmax.py
@@ -0,0 +1,21 @@
+""" Implements softmax activation function. """
+
+import numpy as np
+
+class Softmax():
+    """ Softmax activation function. """
+    def forward(self, x):
+        """ Forward pass of Softmax activation function. """
+        z = x - np.max(x)
+        exp = np.exp(z)
+        probs = exp / np.sum(exp)
+        return probs
+
+    # Make forward method callable lik Softmax(x)
+    __call__ = forward
+
+    def backward(self, y):
+        """ Backward pass of Softmax activation function. """
+        # Create diagonal matrices for entries of y.
+        jacobian = np.diag(y) - np.outer(y, y)
+        return jacobian
