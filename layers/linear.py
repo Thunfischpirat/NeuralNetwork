555af344d42a62d5ba301c877525b145a38c45ac
Christian Singer <christian.singer@mail.schwarz>
2022-12-20 08:33:51 +0100

    - Make gradient updatable.

--- layers/linear.py
+++ layers/linear.py
@@ -7,11 +7,12 @@ class LinearLayer():
         """ Initialize LinearLayer. """
         self.W = np.random.randn(output_dim, input_dim)
         self.b = np.random.randn(output_dim)
+        self.trainable = True
 
     def forward(self, x):
         """ Forward pass of LinearLayer. """
         self.input = x
-        return np.dot(self.W,x) + self.b
+        return np.dot(self.W, x) + self.b
 
     # Make forward method callable like LinearLayer(x)
     __call__ = forward
@@ -22,6 +23,11 @@ class LinearLayer():
         self.grad_W = np.outer(grad_output, x)
         self.grad_b = grad_output
         return np.dot(self.W.T, grad_output)
+
+    def update_params(self, learning_rate):
+        """ Update parameters of LinearLayer. """
+        self.W -= learning_rate * self.grad_W
+        self.b -= learning_rate * self.grad_b
 
 
 
