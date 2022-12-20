39017e91a9881d6945f47f4ccef8df41788a9bfd
Christian Singer <christian.singer@mail.schwarz>
2022-12-20 08:35:09 +0100

    - Enable backward pass through entire model.

--- layers/sequential.py
+++ layers/sequential.py
@@ -17,32 +17,14 @@ class Sequential():
     # Make forward method callable like Sequential(x)
     __call__ = forward
 
-    def backward(self, x, grad_output):
+    def backward(self, grad_output):
         """ Backward pass of Sequential model. """
         for layer in reversed(self.layers):
-            grad_output = layer.backward(x, grad_output)
+            grad_output = layer.backward(grad_output)
         return grad_output
-
-    def params(self):
-        """ Get parameters of Sequential model. """
-        result = []
-        for layer in self.layers:
-            result.extend(layer.params())
-        return result
-
-    def grad_params(self):
-        """ Get gradient of parameters of Sequential model. """
-        result = []
-        for layer in self.layers:
-            result.extend(layer.grad_params())
-        return result
 
     def update_params(self, learning_rate):
         """ Update parameters of Sequential model. """
         for layer in self.layers:
-            layer.update_params(learning_rate)
-
-    def train(self):
-        """ Set model to training mode. """
-        for layer in self.layers:
-            layer.train
+            if layer.trainable:
+                layer.update_params(learning_rate)
