a7a89b54beee236ca8230a51a5dc449905ed13fa
Christian Singer <christian.singer@mail.schwarz>
2022-12-20 08:32:55 +0100

    - Add epsilon to log to prevent numerical issues. - Standardize input of backward method.

--- loss/cross_entropy.py
+++ loss/cross_entropy.py
@@ -4,9 +4,12 @@ import numpy as np
 
 class CrossEntropyLoss():
     """ Cross entropy loss function. """
+    def __init__(self):
+        """ Initialize cross entropy loss function. """
+        self.epsilon = 1e-12
     def forward(self, y, y_hat):
         """ Forward pass of cross entropy loss function. """
-        return -np.sum(y * np.log(y_hat))
+        return -np.sum(y * np.log(y_hat + self.epsilon))
 
     # Make forward method callable like CrossEntropy(y, y_hat)
     __call__ = forward
@@ -20,12 +23,13 @@ class SMCrossEntropyLoss():
     """ cross entropy loss function with build in softmax application. """
     def forward(self, x, y):
         """ Forward pass of softmax cross entropy loss function. """
-        self.y_hat = Softmax()(x)
-        return -np.sum(y * np.log(self.y_hat))
+        y_hat = Softmax()(x)
+        return -np.sum(y * np.log(y_hat))
 
     # Make forward method callable like SMCrossEntropy(x, y)
     __call__ = forward
 
-    def backward(self, y, y_hat):
+    def backward(self, x, y):
         """ Backward pass of softmax cross entropy loss function. """
+        y_hat = Softmax()(x)
         return y_hat - y
